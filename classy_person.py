a0f575b4ffacb421c3c3d43a0b1c91586f7caeae
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-05-20 12:17:55 -0500

    Did the assignment

--- classy_person.py
+++ classy_person.py
@@ -6,10 +6,26 @@ don't forget the use of 'self' and to have the methods:
 3. say_greeting
 4. count_to_age
 '''
+age_counter = 1
 
 class Person:
+    name = ''
+    age = 0
+    age_counter = 1
+
+    def __init__(self, age, name) -> None:
+        self.name = name
+        self.age = age
+    
+    def increase_age(self):
+        self.age = self.age+1
+
+    def say_greeting(self):
+        print(f'Hello world! My name is {self.name}!')
+
+    def count_to_age(self):
+        if self.age >= 1:
+            for x in range(1,self.age+1): print(x)
 
 
-
-
-# You won't need to call anything here.
+# x= Person(35, 'Forest')
